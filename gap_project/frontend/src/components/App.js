ac45d13707a0aac6317a1d5ef3ea4a86704683aa
Juliana Green <2898201g@student.gla.ac.uk>
2024-11-27 15:39:29 +0000

    log in page

---
+++ gap_project/frontend/src/components/App.js
@@ -0,0 +1,22 @@
+import '../css/App.css';
+import Login from './Login'; 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div className="video-container">
+        <video autoPlay muted loop className="background-video" src='/background-video.mp4' type="video/mp4" />
+      </div>
+      <div className="overlay"></div>
+
+        <div className="app-container">
+          <Routes>
+            <Route path="/" element={<Login />} />
+          </Routes>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
--- gap_project/frontend/src/index.js
+++ gap_project/frontend/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/App';
 
 // think of the REACT pages as a tree, the root of this tree is defined here.
 // All of the components are called by eachother, and it starts with one component called : <App />
@@ -14,4 +13,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
