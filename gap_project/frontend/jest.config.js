9b133faf9d5e599ab9a4225d7adc07b214fdde88
Ameer Azlan Shah <2774679a@student.gla.ac.uk>
2025-01-08 00:01:13 +0800

    Modified jest config

--- gap_project/frontend/jest.config.js
+++ gap_project/frontend/jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
     transformIgnorePatterns: [
-      "/node_modules/(?!(reactstrap)/)",
-      "/node_modules/(?!(bootstrap)/)",
-      "/node_modules/(?!(react-hook-form)/)",
-      "/node_modules/(?!(axios)/)",
+      "/node_modules/(?!reactstrap/)",
+      "/node_modules/(?!bootstrap/)",
+      "/node_modules/(?!react-hook-form/)",
+      "/node_modules/(?!axios/)",
     ],
     moduleNameMapper: {
         '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
