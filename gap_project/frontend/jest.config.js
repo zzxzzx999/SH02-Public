81427542f3cfd86d2df1281da25aad325a791de0
Ameer Azlan Shah <2774679a@student.gla.ac.uk>
2025-01-07 23:20:53 +0800

    Added jest and babel config files

---
+++ gap_project/frontend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    transformIgnorePatterns: [
+      "/node_modules/(?!(reactstrap)/)",
+      "/node_modules/(?!(bootstrap)/)",
+      "/node_modules/(?!(react-hook-form)/)",
+      "/node_modules/(?!(axios)/)",
+    ],
+  };
+  
