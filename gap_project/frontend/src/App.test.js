b10c65447a762eb4c7abb08c423024d73a709e42
Ameer Azlan Shah <2774679a@student.gla.ac.uk>
2025-01-08 02:19:10 +0800

    updated App.js

--- gap_project/frontend/src/App.test.js
+++ gap_project/frontend/src/App.test.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import { act } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './components/App';
 
 test('renders learn react link', () => {
-  act(() => {
-    render(<App />);
-  });
-  
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
