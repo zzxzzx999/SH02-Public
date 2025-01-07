d54803ead5973f333fa980d39d57ac45ed35ab3b
Juliana Green <2898201g@student.gla.ac.uk>
2025-01-07 17:47:42 +0000

    basic search bar complete

--- gap_project/frontend/src/components/Home.js
+++ gap_project/frontend/src/components/Home.js
@@ -1,7 +1,10 @@
-import React from "react";
-import NavBar from './NavBar';  // Import the Navbar component
+import React, {useEffect, useState} from "react";
+import NavBar from './NavBar'; 
 import '../css/NavBar.css';
 import '../css/Home.css';
+import debounce from 'lodash/debounce'; //debounce to prevent search everytime searchText changes
+
+const url = 'http://127.0.0.1:8000/api'
 
 
 function AboutUs() {
@@ -9,11 +12,55 @@ function AboutUs() {
     { name: 'Add New Company', path: '/new-company' , image:'/add-new-company.png'},
   ];
 
+  const [data, setData] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  const fetchData = async() => {
+    const token = localStorage.getItem("authToken");
+    const endpoint = `${url}/companies/?name=${searchText}`
+
+    try {
+      const response = await fetch (endpoint, {
+        method: 'GET'
+      })
+
+      const data = await response.json()
+      console.log(data)
+
+      if (Array.isArray(data)) {
+        setData(data);
+      } else {
+          console.error("Expected an array but received:", data);
+      }
+
+      setData(data)
+    }
+    catch (e) {
+      console.error("Error fetching data:", e);
+    }
+  }
+
+  useEffect(()=> {
+    fetchData()
+  }, [searchText])
+
+  const handleSearch = debounce((value) => setSearchText(value), 500); //500ms seach delay
+
   return (
     <div className="main-content">
       <NavBar links={linksForPage1} logout={true}/> {/* Passing the links to the Navbar component */}
       <div className="about-us-search">
         <h2>Search for Company</h2>
+        <input type="search" placeholder="search for company" value={searchText} onChange={e => setSearchText(e.target.value)}/>
+        <ul>
+          {Array.isArray(data) && data.length > 0 ? (
+          data.map((company) => (
+              <li key={company.name}>{company.name}</li>
+          ))
+          ) : (
+          <li>No companies found</li>
+          )}
+        </ul>
       </div>
       <div className="about-us-box">
         <h1>About Us</h1>
