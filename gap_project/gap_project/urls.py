469264003232d9cb54c2aa0ce1bafd41f5f82da4
Juliana Green <2898201g@student.gla.ac.uk>
2025-01-16 15:09:50 +0000

    started sending data to db but need gapid

--- gap_project/gap_project/urls.py
+++ gap_project/gap_project/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path("api/companies/", company_list, name="company_list"),
     path('api/scores/<str:company_name>/<str:element_name>/', views.get_scores, name='get_scores'),
     path('api/overall-scores/<str:company_name>/', views.overall_scores, name='overall_scores'),
-path('api/getQuestionOrWriteAnswer/', views.getQuestionOrWriteAnswer, name='getQuestionOrWriteAnswer'),
+    path('api/getQuestionOrWriteAnswer/', views.getQuestionOrWriteAnswer, name='getQuestionOrWriteAnswer'),
 
 
 ]
