ce7672a9736c74100f7b937865411a730730970a
2784305z <2784305z@student.gla.ac.uk>
2025-01-28 03:07:37 +0800

    delete button fixed

--- gap_project/gap/urls.py
+++ gap_project/gap/urls.py
@@ -13,7 +13,7 @@ path('api/scores/<str:company_name>/<str:element_name>/', views.get_scores, name
 path('api/overall-scores/<str:company_name>/<int:gap_id>/', views.overall_scores, name='overall_scores'),
 path('api/getQuestionOrWriteAnswer/', views.getQuestionOrWriteAnswer, name='getQuestionOrWriteAnswer'),
 path('api/companies/', views.CompanyListView.as_view(), name='company_list'),
-path('api/companies/<str:company_name>/delete', views.CompanyDeleteView.as_view(), name='delete_company'),
+path('api/companies/<str:company_name>/delete/', views.CompanyDeleteView.as_view(), name='delete_company'),
 path('api/companies/<str:company_name>/', views.company_detail, name='company_detail'),
 ]
  
