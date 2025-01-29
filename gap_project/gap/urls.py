fb563099342a0a447b487a80f4397fd670d74461
2771954H <2771954H@student.gla.ac.uk>
2025-01-29 12:25:15 +0000

    Updated urls- need to get this branch in line with 'main'

--- gap_project/gap/urls.py
+++ gap_project/gap/urls.py
@@ -9,7 +9,7 @@ app_name = 'gap'
 urlpatterns = [
 path('', views.index, name='index'),
 path('pdfplan/', views.PdfView.as_view(), name='PdfPlan'),
-path('creategap/', views.create_gap(), name='CreateGap'),
+path('api/create-gap/', views.create_gap, name='create_gap'),
 path('api/scores/<str:company_name>/<str:element_name>/', views.get_scores, name='get_scores'),
 path('api/overall-scores/<str:company_name>/', views.overall_scores, name='overall_scores'),
 ]
