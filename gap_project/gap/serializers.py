0536941d3f2af8a0535202edbe681c992f2125bd
Jessica Neill <2797149n@student.gla.ac.uk>
2025-01-29 12:01:12 +0000

    Update serializers.py GapAnalysis Serializer with the correct fields, delted index serializer

--- gap_project/gap/serializers.py
+++ gap_project/gap/serializers.py
@@ -12,10 +12,12 @@ class CompanySerializer(serializers.ModelSerializer):
             fields = ('name',)
         
 class GapAnalysisSerializer(serializers.ModelSerializer):
-    
+    gap_data = serializers.DictField(required=False)  #validates gap_data is a dictionary
+
     class Meta:
         model = GapAnalysis
-        fields = ['title', 'improvement_plan']
+        fields = ('title','company', 'consultant','companyRep', 'companyEmail','additionalNotes', 'gap_data', 'improvement_plan')
+
 
 class QuestionsSerializer(serializers.Serializer):
     GetOrWrite = serializers.CharField(max_length=10, required=False)
