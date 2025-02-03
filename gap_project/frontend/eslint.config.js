a4567114fe3d442fac371eb9f124612f6b5c1c48
Ameer Ridhuan <ameeridhuan91@gmail.com>
2025-02-03 22:30:10 +0000

    Added eslint confid and .gitlab.ci.yml configuration

---
+++ gap_project/frontend/eslint.config.js
@@ -0,0 +1,11 @@
+export default [
+    {
+      files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+      rules: {
+        "no-unused-vars": "warn",
+        "no-console": "warn",
+        "react/jsx-uses-react": "error"
+      }
+    }
+  ];
+  
