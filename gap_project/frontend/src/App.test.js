70bf27a1ebf50cd7afe434d46c9fff2ea1085d36
Ameer Ridhuan <ameeridhuan91@gmail.com>
2025-02-03 21:17:23 +0000

    Added a temporary test file, updated App.test and package.json

--- gap_project/frontend/src/App.test.js
+++ gap_project/frontend/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './components/App';
 
-test('renders learn react link', () => {
+test('renders login button', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const loginButton = screen.getByText(/log in/i);  // Looks for "LOG IN" button
+  expect(loginButton).toBeInTheDocument();
 });
