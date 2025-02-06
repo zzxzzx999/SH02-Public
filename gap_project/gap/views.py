68bba4c8e41bf142d8084e0a008b6ec2daacbb13
Juliana Green <2898201g@student.gla.ac.uk>
2025-02-06 18:19:45 +0000

    previous commit didnt work due to bug, fixing

--- gap_project/gap/views.py
+++ gap_project/gap/views.py
@@ -61,7 +61,7 @@ def create_gap(request):
     company_rep = request.data.get('company_rep')
     company_email = request.data.get('company_email')
     additional_notes = request.data.get('additional_notes')
-    
+
     try:
         company_instance = Company.objects.get(name=company_name)
         gap = GapAnalysis.objects.create(
@@ -79,19 +79,27 @@ def create_gap(request):
 
     except Company.DoesNotExist:
         return Response({"error": "Company not found"}, status=404)
-    
+
 @api_view(['GET'])
 def get_latest_gap(request):
     company_name = request.GET.get('company_name')
     
     if not company_name:
         return Response({"error": "Company name is required."}, status=400)
-    
-    latest_gap = GapAnalysis.objects.filter(company=company_name).order_by('-id').first()
 
-    if latest_gap:
-        return Response({"gap_id": latest_gap.id})
-    return Response({"error": "No gap analysis found for this company."}, status=404)
+    try:
+        company_instance = Company.objects.get(name=company_name)
+        
+        latest_gap = GapAnalysis.objects.filter(company=company_instance).order_by('-id').first()
+
+        if latest_gap:
+            return Response({"gap_id": latest_gap.id})
+        else:
+            return Response({"error": "No gap analysis found for this company."}, status=404)
+
+    except Company.DoesNotExist:
+        return Response({"error": "Company not found."}, status=404)
+
 
 class PdfView(APIView):
     
