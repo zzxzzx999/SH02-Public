56c3e99187bf958cd1013ad47673d53175d58ce2
Juliana Green <2898201g@student.gla.ac.uk>
2025-02-15 20:04:17 +0000

    added final styling + logic

--- gap_project/frontend/src/components/Accordion.js
+++ gap_project/frontend/src/components/Accordion.js
@@ -2,6 +2,22 @@ import React, { useRef, useState, useEffect } from "react";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import '../css/Accordion.css'
 
+const COLOR_MAP = {
+    exceptional: '#006613', 
+    good: '#42C259',
+    basic: '#7CCC8B', 
+    needsImprovement: '#FFC546', 
+    unsatisfactory: '#FF0B0B', 
+  };
+
+  const compliance = {
+    1: {text:( <> Exceptional Compliance<br /> (Score 5) </> ), color: '#006613'},
+    2: {text:( <> Good Compliance<br /> (Score 4) </> ), color: '#42C259'},
+    3: {text:( <> Basic Compliance<br /> (Score 3) </> ), color: '#7CCC8B'},
+    4: {text:( <> Needs Improvement<br /> (Score 2) </> ), color: '#FFC546'},
+    5: {text:( <> Unsatisfactory<br /> (Score 1) </> ), color: '#FF0B0B'}
+  };
+
 //  accordionitem component
 const AccordionItem = ({ question, score, improvement, evidence, isOpen, onClick, index }) => {
   const contentHeight = useRef();
@@ -25,9 +41,12 @@ const AccordionItem = ({ question, score, improvement, evidence, isOpen, onClick
             : { height: "0px" }
         }
       >
-        <p className="answer-content">Compliance Score: {score}</p>
-        <p className="answer-content">Evidence: {evidence}</p>
-        <p className="answer-content">Improvement: {improvement}</p>
+        <p className="compliance-box" style={{backgroundColor:compliance[score]?.color}}>{compliance[score]?.text || "No score"}</p>
+
+        <div className="evidence-improvement-container">
+            <p className="evidence-text">{evidence || "N/A"}</p>
+            <p className="improvement-text">{improvement || "N/A"}</p>
+        </div>
 
       </div>
     </div>
