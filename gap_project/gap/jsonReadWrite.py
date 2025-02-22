53248652ad7ed3eea5643773a1460cc7fea45b8a
2771954H <2771954H@student.gla.ac.uk>
2025-02-22 17:33:51 +0000

    Changed json read write functions to fit with correctly formatted json data

--- gap_project/gap/jsonReadWrite.py
+++ gap_project/gap/jsonReadWrite.py
@@ -3,20 +3,20 @@ import os
 from django.conf import settings
 
 def getAnswer(element, question, gapAnalysis):
-    return json.loads(gapAnalysis.gap_data)[str(element)][question-1]
+    return gapAnalysis.gap_data[str(element)][question-1]
 
 def getElementAnswers(element, gapAnalysis):
-    return json.loads(gapAnalysis.gap_data)[str(element)]
+    return gapAnalysis.gap_data[str(element)]
     
 def writeAnswer(answer, element, question, gapAnalysis):
-    data = json.loads(gapAnalysis.gap_data)
+    data = gapAnalysis.gap_data
     data[str(element)][question-1] = answer
-    gapAnalysis.gap_data = json.dumps(data)
+    gapAnalysis.gap_data = data
     gapAnalysis.save()
     
 
 def getImprovementAnswer(element, question, gapAnalysis):
-    return json.loads(str(gapAnalysis.improvement_plan))[str(element)][question-1]
+    return gapAnalysis.improvement_plan[str(element)][question-1]
 
 def getAllElement(element):
     print("PINK PONY GIRLS")
@@ -37,12 +37,7 @@ def getElementHeading(element):
     return getAllElement(element)["Section_Name"]
 
 def getAllAnswersSingleGap(element, question, gapAnalysis):
-    return json.loads(gapAnalysis.gap_data)
+    return gapAnalysis.gap_data
 
 def pdfFormatElement(gap, element):
-    
-    plan = json.loads(str(gap.improvement_plan))
-    print(gap.company)
-    print(gap.date)
-    print(plan)
-    return plan[str(element)]
+    return gap.improvement_plan[str(element)]
