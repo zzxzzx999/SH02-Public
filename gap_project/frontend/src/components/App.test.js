5a3917e0649dcabc736fb11b10317d3c146bf640
Ameer Ridhuan <ameeridhuan91@gmail.com>
2025-02-26 15:11:10 +0000

    Removed CreateNewCompany.js and Results.js (both unused), updated App.js and moved App.test.js to a new test folder

--- gap_project/frontend/src/components/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders login button', () => {
-  render(<App />);
-  const loginButton = screen.getByText(/log in/i);  // Looks for "LOG IN" button
-  expect(loginButton).toBeInTheDocument();
-});
--- gap_project/frontend/src/components/Results.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function Results(){
-    return(
-        <p>This is the results page</p>
-    );
-}
-
-export default Results;
