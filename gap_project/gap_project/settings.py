a4cfbd33d44e31bd35fd6ad28ff8c3f47c5df580
Juliana Green <2898201g@student.gla.ac.uk>
2025-03-03 10:21:32 +0000

    frontend link works!!! need to fix db

--- gap_project/gap_project/settings.py
+++ gap_project/gap_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = "django-insecure-a+&w_tt6jw$@xx$((ri1(&2+p$)=kg^21nsul9ec%(^*orrgjt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["gordon-foley-backend.onrender.com"]
+ALLOWED_HOSTS = ["gordon-foley-backend.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
